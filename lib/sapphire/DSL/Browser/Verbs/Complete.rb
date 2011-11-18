09dfa1cb6fc4c89f9cb8f0f04d2880da40bd6654
ot-chris-holt <cholt@onetechnologies.net>
2011-11-18 16:28:03 -0600

    fixed complete to work with Complete MemberLogin

--- lib/sapphire/DSL/Browser/Verbs/Complete.rb
+++ lib/sapphire/DSL/Browser/Verbs/Complete.rb
@@ -2,9 +2,13 @@ module Sapphire
   module DSL
     module Browser
       def Complete(page)
-        @page = page.Complete
+        if(page.is_a? Class)
+          temp = page.new
+          @page = temp.Complete
+        else
+          @page = page.Complete
+        end
       end
     end
   end
 end
-
