9f72128773490b7d18734d8b2509f3540748bf89
ot-chris-holt <cholt@onetechnologies.net>
2012-01-19 16:49:09 -0600

    Added attempt verb

---
+++ lib/sapphire/DSL/Browser/Verbs/Attempt.rb
@@ -0,0 +1,14 @@
+module Sapphire
+  module DSL
+    module Browser
+      def Attempt(page)
+        if(page.is_a? Class)
+          temp = page.new
+          @page = temp.Attempt
+        else
+          @page = page.Attempt
+        end
+      end
+    end
+  end
+end
