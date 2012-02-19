d93593b5d0b4df6824d9611055ef166017ebc265
Marcus Bratton <marcus.bratton@gmail.com>
2012-02-18 23:48:38 -0600

    refactoring

--- lib/sapphire/DSL.rb
+++ lib/sapphire/DSL.rb
@@ -2,6 +2,7 @@ require File.expand_path(File.dirname(__FILE__) +'/DSL/Evaluators/Evaluator.rb',
 require File.expand_path(File.dirname(__FILE__) +'/DSL/Comparisons/Comparison.rb', __FILE__)
 Dir[File.dirname(__FILE__) + '/DSL/Evaluators/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/DSL/Comparisons/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/DSL/Evaluations/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/DSL/TestPlans/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/DSL/Browser/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/DSL/Browser/Fluff/*.rb'].each {|file| require file }
