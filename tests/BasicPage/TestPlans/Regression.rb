f20cd6f58539f5c65236a25a51c7daa8469fe49a
Marcus Bratton <mbratton@onetechnologies.net>
2012-03-20 18:24:51 -0500

    Signed-off-by: Marcus Bratton <mbratton@onetechnologies.net>

--- tests/BasicPage/TestPlans/Regression.rb
+++ tests/BasicPage/TestPlans/Regression.rb
@@ -14,12 +14,13 @@ TestPlan "Sapphire Regression" do
     Run :path => :specs_path + "/Controls/"
     Run :path => :specs_path + "/Verbs/"
     Run :path => :specs_path + "/Evaluators/"
-    Run :path => :specs_path + "/LanguageFeatures/"
 
     Create Matrix :test_grid do
-      Using :user_first_name => "John",
+      Using :description => "Test 1",
+            :user_first_name => "John",
             :user_last_name => "Doe"
-      Using :user_first_name => "John222",
+      Using :description => "Test 2",
+            :user_first_name => "John222",
             :user_last_name => "Doe"
     end
 
@@ -27,6 +28,7 @@ TestPlan "Sapphire Regression" do
       Run :file => :specs_path + "/Matrix/MatrixSpecs.rb"
     end
 
+    Run :path => :specs_path + "/LanguageFeatures/"
     #todo - /Verbs/MouseOver
     #todo - /Verbs/Switch
 
