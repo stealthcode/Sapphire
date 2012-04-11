c532c4ea38e5a8a6ce29b5d0f52e5af5640cb8b7
Marcus Bratton <mbratton@onetechnologies.net>
2012-04-11 10:49:41 -0500

    Signed-off-by: Marcus Bratton <mbratton@onetechnologies.net>

--- lib/sapphire/TeamCity.rb
+++ lib/sapphire/TeamCity.rb
@@ -1,9 +1,11 @@
-Dir[File.dirname(__FILE__) + '/TeamCity/*.rb'].each {|file| require file } if ENV["TeamCity"] == "1"
-
 module Sapphire
   module Testing
     module TeamCity
+      class TeamCityReporter < Reporter
 
+      end
     end
   end
 end
+
+Dir[File.dirname(__FILE__) + '/TeamCity/*.rb'].each {|file| require file } if ENV["TeamCity"] == "1"
