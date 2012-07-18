667431de59d8cc73b265d879465788cf6416cd1a
Marcus Bratton <marcus.bratton@gmail.com>
2012-07-17 23:55:53 -0500

    constraining debug output

--- tests/BasicPage/Specs/Controls/TextBoxSpecs.rb
+++ tests/BasicPage/Specs/Controls/TextBoxSpecs.rb
@@ -10,7 +10,7 @@ Scenario "Testing the TextBox control" do
       Should Show BasicPage
     end
       Then "the first name is set to John" do
-        Should Show :first_name => "John"
+        Should Show :first_name => "John1"
       end
   #-------------------------------------------------------------------------------
     When "the user is on the page" do
